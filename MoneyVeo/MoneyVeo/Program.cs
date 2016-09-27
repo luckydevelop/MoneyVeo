9e9067e2e6f1955a15ce1e59562d5b74f5280abf
oleg <olegmarket@gmail.com>
2016-09-27 16:57:24 +0300

    final changes

--- MoneyVeo/MoneyVeo/Program.cs
+++ MoneyVeo/MoneyVeo/Program.cs
@@ -6,33 +6,46 @@ using System;
 
 namespace MoneyVeo
 {
+    [TestFixture, RequiresSTA]
     internal class Program
     {
-        private static readonly IWebDriver Driver = new FirefoxDriver();
+        private IWebDriver _driver;
 
-        private static readonly By ButtonSearch = By.XPath(".//*[@id='sblsbb']/button");
-        private static readonly By FieldSearch = By.XPath(".//input[@id='lst-ib']");
-        private static readonly By TextForthResultHeader = By.XPath(".//*[@id='rso']/div/div[4]/div/h3/a");
+        private readonly By _buttonSearch = By.XPath(".//*[@id='sblsbb']/button");
+        private readonly By _fieldSearch = By.XPath(".//input[@id='lst-ib']");
+        private readonly By _textForthResultHeader = By.XPath(".//*[@id='rso']/div/div[4]/div/h3/a");
 
         private const string ExpectedResult = "Selenium IDE";
         private const string PageAdres = "http://www.google.com";
         private const string SearchingInquiry = "Selenium IDE export to C#";
 
-        private static void Main(string[] args)
+        [TestFixtureSetUp]
+        public void Init()
         {
-            Driver.Manage().Window.Maximize();
-            Driver.Url = PageAdres;
-            FindElementWithWait(FieldSearch).SendKeys(SearchingInquiry);
-            FindElementWithWait(ButtonSearch).Click();
-            string actualResult = FindElementWithWait(TextForthResultHeader).Text;
+            _driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void MoneyVeoTest()
+        {
+            _driver.Manage().Window.Maximize();
+            _driver.Url = PageAdres;
+            FindElementWithWait(_fieldSearch).SendKeys(SearchingInquiry);
+            FindElementWithWait(_buttonSearch).Click();
+            string actualResult = FindElementWithWait(_textForthResultHeader).Text;
             bool res = actualResult.Contains(ExpectedResult);
-            //Assert.IsTrue(res, $"Actual result '{actualResult}' should contain next subString '{ExpectedResult}'");
             Assert.IsTrue(res, "Actual result '" + actualResult + "'should contain next subString '" + ExpectedResult + "'");
         }
 
-        private static IWebElement FindElementWithWait(By locator)
+        [TestFixtureTearDown]
+        public void Cleanup()
         {
-            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromMilliseconds(5000));
+            _driver.Close();
+        }
+
+        public IWebElement FindElementWithWait(By locator)
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromMilliseconds(5000));
             IWebElement element = wait.Until(ExpectedConditions.ElementIsVisible(locator));
             return element;
         }
